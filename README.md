9c632b0cd850c6b345d78e9b128ad18cb09c328f
yurii.derevenko <yurii.derevenko@globalorange.nl>
2018-02-07 16:48:14 +0200

    feat(initial commit):

---
+++ README.md
@@ -0,0 +1,1 @@
+# SQL query loader
---
+++ index.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+const path = require('path')
+const util = require('util')
+
+const readDirAsync = util.promisify(fs.readdir)
+const readFileAsync = util.promisify(fs.readFile)
+
+const queries = new Map()
+
+class SqlManager {
+  static async loadDir (dirName) {
+    try {
+      const fileNames = await readDirAsync(dirName)
+
+      for (const fileName of fileNames) {
+        const fileNameWithoutExt = path.basename(fileName, '.sql')
+        const file = await readFileAsync(path.join(dirName, fileName), 'utf-8')
+
+        queries.set(fileNameWithoutExt, file)
+      }
+    } catch (err) {
+      console.log(err)
+      throw err
+    }
+  }
+
+  static async loadFile (fileName) {
+    try {
+      const fileNameWithoutExt = path.basename(fileName, '.sql')
+      const file = await readFileAsync(fileName, 'utf-8')
+
+      queries.set(fileNameWithoutExt, file)
+    } catch (err) {
+      console.log(err)
+      throw err
+    }
+  }
+
+  static getQuery (queryName) {
+    return queries.get(queryName)
+  }
+}
+
+module.exports = SqlManager
